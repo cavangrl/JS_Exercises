6c1210098daed794bfc2144a4a095bc0e33f41c7
cavangrl <pae.oconnor@gmail.com>
2014-09-30 20:16:32 -0400

    exercise

---
+++ exercise.js
@@ -0,0 +1,23 @@
+
+
+function squareNumb(num) {
+    var squaredNumber = num * num;
+    console.log('The result of squaring the number ' + num + ' is ' + squaredNumber);
+    return squaredNumber;
+}
+
+squareNumb(5);
+
+function halfOf(num){
+var half = num/2;
+ console.log('Half of ' + num + ' is ' +  half);
+return half;
+}
+halfOf(20);
+
+function percentOf(num1, num2){
+    var percent = (num1/num2) * 100;
+    console.log(num1 + ' is ' + percent + '% of ' + num2);
+    return percent;
+}
+percent(2, 4);
